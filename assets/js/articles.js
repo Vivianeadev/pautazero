8b0122f717f7b58500dbca59c4fb86cb39da379f
Vivianeadev <avizaecosystem@gmail.com>
2026-04-13 00:34:00 -0300

    Create articles.js

---
+++ assets/js/articles.js
@@ -0,0 +1,23 @@
+const articles = [
+  {
+    titulo: "Psicopatia no poder",
+    url: "/artigo/polemicas/psicopatia-no-poder.html",
+    tema: "psicopatia",
+    subtema: "poder",
+    tags: ["liderança", "manipulação"],
+    data: "2026-04-12",
+    destaque: true,
+    imagem: "/assets/img/psicopatia.jpg"
+  },
+
+  {
+    titulo: "Tokenização imobiliária",
+    url: "/artigo/economia/tokenizacao-imobiliaria.html",
+    tema: "economia",
+    subtema: "tokenizacao",
+    tags: ["blockchain", "imóveis"],
+    data: "2026-04-10",
+    destaque: false,
+    imagem: "/assets/img/token.jpg"
+  }
+];
